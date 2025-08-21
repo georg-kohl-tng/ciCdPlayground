153e33e504224b3b3d2b3a6e12c2b5d240f255b2
Georg Kohl <georg.kohl@tngtech.com>
2025-08-21 20:07:35 +0200

    Add contents

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Georg Kohl",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["Code In / Calamity Deployed"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "Github Actions, Argo CD",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["Things about CI/CD"],
   },
 });
